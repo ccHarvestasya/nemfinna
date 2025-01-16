a43388134e9b805d9ac83f34923120318a51c7c6
ccHarvestasya <harvestasya84@gmail.com>
2025-01-16 22:05:05 +0900

    AppServiceにSssFetchを統合し、アプリケーション起動時にAPIノードキャッシュを更新する機能を追加

--- syanoa/src/app.service.ts
+++ syanoa/src/app.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common'
+import { SssFetch } from 'sss-fetch'
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name)
+
+  async onApplicationBootstrap(): Promise<void> {
+    this.logger.log(`The module has been initialized.`)
+    const sssFetch = new SssFetch('testnet')
+    await sssFetch.tryRefreshApiNodesCache()
+    const apiNodes = sssFetch.randomApiNodes(true, 5)
+    console.log(apiNodes)
+  }
+
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!'
   }
 }
