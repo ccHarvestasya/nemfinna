0d10571f189efe5caa6775619420b0a3c7fa7b8a
ccHarvestasya <harvestasya84@gmail.com>
2025-01-18 00:03:27 +0900

    環境設定ファイルを追加し、ConfigModuleを導入して環境変数の管理を改善

--- xym-price/src/app.module.ts
+++ xym-price/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller.js'
 import { AppService } from './app.service.js'
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: [`.env.${process.env.NODE_ENV}`, `.env`] })],
   controllers: [AppController],
   providers: [AppService],
 })
--- xym-price/src/app.service.ts
+++ xym-price/src/app.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, Logger, OnApplicationBootstrap } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { SymbolWs, WsBlock } from 'symbol-ws'
 
 @Injectable()
@@ -6,26 +7,38 @@ export class AppService implements OnApplicationBootstrap {
   private readonly logger = new Logger(AppService.name)
   private symbolWs: SymbolWs
 
-  async onApplicationBootstrap() {
-    this.logger.log(`The module has been initialized.`)
+  constructor(private readonly config: ConfigService) {}
 
-    this.symbolWs = new SymbolWs('testnet')
+  /**
+   * すべてのモジュールが初期化された後
+   */
+  async onApplicationBootstrap() {
+    this.logger.log('  _/_/_/_/_/_/_/_/_/_/_/')
+    this.logger.log(` _/_/ ${process.env.NODE_ENV.toUpperCase().padStart(12, ' ')} _/_/`)
+    this.logger.log('_/_/_/_/_/_/_/_/_/_/_/')
+
+    this.symbolWs = new SymbolWs(this.config.get('NETWORK_TYPE'))
     await this.symbolWs.connect()
 
     this.symbolWs.on('open', (wsUrl) => {
-      this.logger.debug('WebSocket接続:', wsUrl)
+      this.logger.debug(`WebSocket接続: ${wsUrl}`)
       this.logger.debug(this.symbolWs.subscribe('block'))
     })
 
     this.symbolWs.on('block', (blcok: WsBlock) => {
-      this.logger.debug(blcok.data.block.height, blcok.data.meta.hash)
+      this.logger.debug(`${blcok.data.block.height} ${blcok.data.meta.hash}`)
     })
 
     this.symbolWs.on('reconnect', (code: number, reason: string) => {
-      this.logger.debug('reConnect:', code, reason.toString())
+      this.logger.debug(`reConnect: ${code} ${reason.toString()}`)
     })
 
-    this.logger.debug('websocketUrl:', await this.symbolWs.websocketUrl)
+    const wsUrlPath = await this.symbolWs.websocketUrl
+    const wsUrl = new URL(wsUrlPath)
+    const host = wsUrl.hostname
+    const port = wsUrl.port
+    this.logger.debug(`websocketUrl  : ${wsUrlPath}`)
+    this.logger.debug(`restGatewayUrl: https://${host}:${port}`)
   }
 
   getHello(): string {
